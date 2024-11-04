d0ed7b2f1b82af3b69d74ad45f341eba78b72818
Nguyen Hoang Hien <hien20040908@gmail.com>
2024-11-04 11:59:39 +0700

    Remove configs

--- Assets/Scripts/Gameplay/Configs/PowerUpBase.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class PowerUpBase : ScriptableObject
-{
-    public Sprite Sprite;
-    public string Description;
-
-    public abstract void SetUpPowerUp();
-}
