dd55256fd46d6c5375c2f4b9ecb53f317f86df66
Nguyen Hoang Hien <hien20040908@gmail.com>
2024-11-05 15:31:11 +0700

    Add toggle loading screen

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -3,15 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : SingletonPersistent<UIManager>
 {
-    [SerializeField] private GameObject _gameOverScreen;
+    [SerializeField] private GameObject _gameOverScreen, _loadingScreen;
 
     public void LoadGame()
     {
         SceneManager.LoadScene("Gameplay");
+        ToggleLoadingScreen();
     }
 
     public void ToggleGameOverScreen()
     {
         _gameOverScreen.SetActive(!_gameOverScreen.activeSelf);
+    }
+
+    public void ToggleLoadingScreen()
+    {
+        _loadingScreen.SetActive(!_loadingScreen.activeSelf);
     }
 }
